012958d7e84ea468cf1e7e62be2adb540be9a019
yuukikuskai <2466286500@qq.com>
2020-03-03 22:53:48 +0800

    3/3 牌型思路

--- src/js/fight_the_landlords/pokerRule.js
+++ src/js/fight_the_landlords/pokerRule.js
@@ -11,6 +11,8 @@
     3：三张
     4：三带一、炸弹
     5：顺子、三带二
+---------------------------------------------
+         顺子（单，双，三） 飞机（单、对）
     6：顺子、连对、三连对、四带二
     7：顺子
     8：顺子、连对、飞机带单、四带二对
@@ -25,47 +27,78 @@
     17：飞机带单
 */
 /*
-    1. 一个方法判断一个里面重复两次的 m 个，三次的 m 个
+    {
+        一次:
+        二次:
+        三次:
+        四次:
+    }
+    function 判断连续(){
+        if(arr[0]-arr[arr.length-1]==arr.length-1){
+            return 连续
+        }
+    }
+    大于 5 时
+    1. 一个方法判断一个里面一次的有i个重复两次的 j 个，三次的 k 个
     2. 一个方法判断顺子
     3. 重复两次的有 m 个
-    4. if(m==len && 顺子判断) 顺子
-    5. if(m==len/2 && 顺子判断) 连对
-    6. 
+    if(j==0&&k==0){
+        判断连续
+        return 顺子
+    }else if(j*2==len){
+        判断连续(m)
+        return 连对
+    }else if(k*3==len){
+        判断连续(n)
+        return 三连对
+    }else {
+        if(k==0 || 判断连续(k)==false){
+            错误
+        }
+        if(2*i+k*3==len){
+            return 飞机带单
+        }
+        else if(2*j+k*3==len){
+            return 飞机带双
+        }else{
+            return 错误
+        }
+    }
 */
 
-function cardType(card){
+function cardType(card) {
     let type = null;
     const len = card.length;
-    if (len == 1){
+    if (len == 1) {
         type = 'single';
     }
-    if(len == 2){
-        if(card[0].point == card[1].point){
+    if (len == 2) {
+        if (card[0].point == card[1].point) {
             type = 'pair';
-        }else{
+        } else {
             type = 'err';
         }
     }
-    if(len == 3){
-        if(card[0].point == card[1].point&&card[1].point == card[2].point){
+    if (len == 3) {
+        if (card[0].point == card[1].point && card[1].point == card[2].point) {
             type = 'triple';
-        }else{
+        } else {
             type = 'err';
         }
     }
-    if(len == 4){
-       if(card[1].point==card[2].point){
-           if((card[0].point==card[1].point||card[1].point==card[3].point)
-           &&card[0].point!=card[3].point){
-               type = 'twa';
-           }else if(card[0]==card[1]&&card[1]==card[3]){
-               type = 'boom';
-           }else{
-               type = 'err';
-           }
-       }
+    if (len == 4) {
+        if (card[1].point == card[2].point) {
+            if ((card[0].point == card[1].point || card[1].point == card[3].point)
+                && card[0].point != card[3].point) {
+                type = 'twa';
+            } else if (card[0] == card[1] && card[1] == card[3]) {
+                type = 'boom';
+            } else {
+                type = 'err';
+            }
+        }
     }
-    
+
     return type;
 }
 
